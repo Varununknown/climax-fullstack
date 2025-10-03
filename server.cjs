bab43ae000fca3558cbe71a24aed5b59da3ed69f
iamgodisgreat <varununknownu@gmail.com>
2025-10-03 23:55:40 +0530

    fix: remove deprecated bufferMaxEntries option

--- server.cjs
+++ server.cjs
@@ -44,8 +44,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  maxPoolSize: 5,
-  bufferMaxEntries: 0
+  maxPoolSize: 5
 })
   .then(() => {
     console.log('✅ Connected to MongoDB Atlas');
